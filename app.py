141bd5a8a5f694e376744303dd8395b95e9b3b19
123nush <anushkadal1611@gmail.com>
2024-02-19 22:56:20 +0530

    skill wise analysis done

--- app.py
+++ app.py
@@ -22,17 +22,21 @@ def home():
 def predict():
     if all(key in request.form for key in ('job_profile_name_analysis', 'attained_questions_analysis', 'score_analysis', 'category_performance_analysis')):
         job_profile_name = request.form['job_profile_name_analysis']
+        job_list = [job_profile_name]
         attained_questions_analysis = float(request.form['attained_questions_analysis'])
         if attained_questions_analysis == 0:
             attained_questions_analysis = 1
         score_analysis = float(request.form['score_analysis'])
         correctness = round((score_analysis * 100) / attained_questions_analysis, 2)
         label_encoder = LabelEncoder()
-        # Map job profile name to integer using label encoding
+        
+        # Fit the LabelEncoder to the training data for job profiles
+        if not hasattr(label_encoder, 'classes_'):
+            label_encoder.fit(job_list)
         job_profile_name_analysis = label_encoder.transform([job_profile_name])[0]
         
         # Initialize output string
-        output = f'Performance for {job_profile_name} is '
+        output = f'Overall Performance for {job_profile_name} is '
 
         # Predict performance for the job profile
         data_to_predict = [attained_questions_analysis, score_analysis, correctness, job_profile_name_analysis]
@@ -44,8 +48,10 @@ def predict():
         performance_labels = ['Needs Improvement', 'Extremely Poor', 'Very Poor', 'Poor', 'Below Average', 'Average',
                               'Above Average', 'Good', 'Very Good', 'Excellent']
         job_profile_performance = performance_labels[predicted_performance[0]]
-        output += f'{job_profile_performance}, '
-
+        output += f'{job_profile_performance} '
+        output+='\n'
+        output+=f'Skill Wise Analysis:'
+     
         # Parse category data and predict performance for each category
         category_data = json.loads(request.form['category_performance_analysis'])
         for category_info in category_data:
@@ -53,16 +59,26 @@ def predict():
             y_count = category_info['Y']
             n_count = category_info['N']
             total_count = category_info['total']
+            category_list = [category_name]
+            
+            # Fit the LabelEncoder to the training data for categories
+            if not hasattr(label_encoder, 'classes_'):
+                label_encoder.fit(category_list)
+            if category_name not in label_encoder.classes_:
+                label_encoder.classes_ = np.append(label_encoder.classes_, category_name)
+            
+            category_name_analysis = label_encoder.transform([category_name])[0]
             
             # Calculate correctness for the category
-            category_correctness = round((y_count / total_count) * 100, 2)
+            category_correctness = round((float(y_count) / float(total_count)) * 100, 2)
             
             # Predict performance based on correctness
-            predicted_category_performance = model.predict([[total_count, y_count, category_correctness, category_name]])[0]
+            predicted_category_performance = model.predict([[total_count, y_count, category_correctness, category_name_analysis]])[0]
             category_performance_label = performance_labels[predicted_category_performance]
             
             # Append category performance to the output string
-            output += f'{category_name} {category_performance_label}, '
+           
+            output += f'\n {category_name} :{category_performance_label}, '
 
         # Remove trailing comma and space
         output = output[:-2]
@@ -70,6 +86,7 @@ def predict():
         return output
     else:
         return 'Required form fields are missing'
+
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     '''
